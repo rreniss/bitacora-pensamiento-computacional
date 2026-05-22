eba27ce77bcf4d23952d10cf2d4ee684c2a2e2b5
rreniss <renatadanyanc2001@gmail.com>
2026-05-22 14:33:27 -0400

    entrega-07.js

--- entregas/Entrega-07.js/entrega-07.js
+++ entregas/Entrega-07.js/entrega-07.js
@@ -1,6 +1,6 @@
 # entrega línea de codigo solemne-1, que exporta mi sketch en una imagen cuando hago click
 
-https://editor.p5js.org/renata.danyan/sketches/NfrrdCoe2
+(https://editor.p5js.org/renata.danyan/sketches/NfrrdCoe2)
 
 function setup() {
   createCanvas(400, 450);
